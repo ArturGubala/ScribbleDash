adb6aa43c394cf42d981d375a5e1aeaa2f37e2d7
arturgie <gubala-artur@wp.pl>
2025-04-01 21:46:34 +0200

    Add fonts: BagelFatOne and Outfit to Type file.

--- app/src/main/java/com/scribbledash/ui/theme/Type.kt
+++ app/src/main/java/com/scribbledash/ui/theme/Type.kt
@@ -1,34 +1,27 @@
 package com.scribbledash.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.scribbledash.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val BagelFatOne = FontFamily(
+    Font(
+        resId = R.font.bagel_fat_one_regular,
+        weight = FontWeight.Normal
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+val Outfit = FontFamily(
+    Font(
+        resId = R.font.outfit_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.outfit_medium,
+        weight = FontWeight.Medium
+    )
+)
+
+val Typography = Typography()
