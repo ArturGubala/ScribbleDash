1c622c8128e6b3c0814860d1d87f9b2faf68ee0b
arturgie <gubala-artur@wp.pl>
2025-04-03 21:27:49 +0200

    Update Manifest. Call core app clas in MainActivity.

--- app/src/main/java/com/scribbledash/MainActivity.kt
+++ app/src/main/java/com/scribbledash/MainActivity.kt
@@ -4,44 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
 import com.scribbledash.ui.theme.ScribbleDashTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, insets -> insets }
+
         setContent {
+            val appState = rememberScribbleDashAppState()
+
             ScribbleDashTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ScribbleDashApp(appState)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScribbleDashTheme {
-        Greeting("Android")
     }
 }
