99a5597b04101629f9215be44c1b0b5419cfc23d
arturgie <gubala-artur@wp.pl>
2025-04-04 20:22:16 +0200

    Add app colors, typography and temporary changes to Theme.

--- app/src/main/java/com/scribbledash/ui/theme/Color.kt
+++ app/src/main/java/com/scribbledash/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.scribbledash.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// TODO: thing about color name conventions and choose one
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val MenuItemSelected = Color(0xFF238CFF)
+val MenuItemUnselected = Color(0xFFE1D5CA)
+
+val Text = Color(0xFF514437)
+val Text2 = Color(0xFF7F7163)
--- app/src/main/java/com/scribbledash/ui/theme/Type.kt
+++ app/src/main/java/com/scribbledash/ui/theme/Type.kt
@@ -1,9 +1,11 @@
 package com.scribbledash.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.scribbledash.R
 
 val BagelFatOne = FontFamily(
@@ -24,4 +26,26 @@ val Outfit = FontFamily(
     )
 )
 
-val Typography = Typography()
+val Typography = Typography(
+    titleMedium = TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp,
+        lineHeight = 30.sp,
+        color = Text
+    ),
+    titleLarge= TextStyle(
+        fontFamily = BagelFatOne,
+        fontWeight = FontWeight.Normal,
+        fontSize = 40.sp,
+        lineHeight = 44.sp,
+        color = Text
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Outfit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        color = Text2
+    ),
+)
