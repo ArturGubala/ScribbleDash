f78190b349ae1ca5bb74d83e00937908d3d330f0
arturgie <gubala-artur@wp.pl>
2025-04-07 21:23:37 +0200

    Add DifficultyLevelNavigation.

---
+++ app/src/main/java/com/scribbledash/core/presentation/screens/difficultylevel/navigation/DifficultyLevelNavigation.kt
@@ -0,0 +1,34 @@
+package com.scribbledash.core.presentation.screens.difficultylevel.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.scribbledash.core.presentation.screens.difficultylevel.DifficultyLevelRoute
+import com.scribbledash.core.presentation.utils.toGameModeTypeUiModel
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToDifficultyLevel(finalDestination: String, navOptions: NavOptions? = null) {
+    navigate(DifficultyLevelScreen(finalDestination), navOptions)
+}
+
+
+fun NavGraphBuilder.difficultyLevelScreen(
+    navController: NavController,
+    onBackClick: () -> Unit
+) {
+    composable<DifficultyLevelScreen> {
+        val args = it.toRoute<DifficultyLevelScreen>()
+        DifficultyLevelRoute(
+            finalDestination = args.finalDestination.toGameModeTypeUiModel().route,
+            navController = navController,
+            onBackClick = onBackClick
+        )
+    }
+}
+
+@Serializable
+class DifficultyLevelScreen(
+    val finalDestination: String
+)
