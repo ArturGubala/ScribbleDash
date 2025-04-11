8d343f1d353b88fd1e8d7975e61e02a1f8ea4668
arturgie <gubala-artur@wp.pl>
2025-04-11 22:04:24 +0200

    Add state class for OneRoundWonderScreen.

---
+++ app/src/main/java/com/scribbledash/gamemodes/oneroundwonder/OneRoundWonderState.kt
@@ -0,0 +1,20 @@
+package com.scribbledash.gamemodes.oneroundwonder
+
+import com.scribbledash.gamemodes.oneroundwonder.model.PathData
+import kotlin.collections.isNotEmpty
+
+data class OneRoundWonderState (
+    val currentPath: PathData? = null,
+    val paths: List<PathData> = emptyList(),
+    val undoPaths: ArrayDeque<PathData> = ArrayDeque(),
+    val redoPaths: ArrayDeque<PathData> = ArrayDeque()
+) {
+    val isClearCanvasButtonActive: Boolean
+        get() = paths.isNotEmpty()
+
+    val isUndoButtonActive: Boolean
+        get() = undoPaths.isNotEmpty()
+
+    val isRedoButtonActive: Boolean
+        get() = redoPaths.isNotEmpty()
+}
---
+++ app/src/main/java/com/scribbledash/gamemodes/oneroundwonder/model/PathData.kt
@@ -0,0 +1,5 @@
+package com.scribbledash.gamemodes.oneroundwonder.model
+
+import androidx.compose.ui.geometry.Offset
+
+data class PathData( val path: List<Offset> )
