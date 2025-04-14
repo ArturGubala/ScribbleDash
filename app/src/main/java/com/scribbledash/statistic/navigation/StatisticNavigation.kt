0dab0d5d3ae735af3a6cbfd5a42797e1e924c721
arturgie <gubala-artur@wp.pl>
2025-04-14 23:19:32 +0200

    Add temporary implementation of StatisticScreen.

--- app/src/main/java/com/scribbledash/statistic/navigation/StatisticNavigation.kt
+++ app/src/main/java/com/scribbledash/statistic/navigation/StatisticNavigation.kt
@@ -1,10 +1,20 @@
 package com.scribbledash.statistic.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.scribbledash.core.presentation.utils.GradientScheme
 import kotlinx.serialization.Serializable
 
 fun NavController.navigateToStatistic(navOptions: NavOptions? = null) = navigate(StatisticScreen, navOptions)
@@ -13,7 +23,22 @@ fun NavGraphBuilder.statisticScreen(
     onBackClick: () -> Unit
 ) {
     composable<StatisticScreen> {
-        Text("Statistic screen")
+        Scaffold(
+            containerColor = Color.Transparent,
+            modifier = Modifier
+                .background(GradientScheme.PrimaryGradient)
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("Statistic screen")
+                Text("Under preparation")
+            }
+        }
     }
 }
 
