4ee06b69fb1e00963cba664e3973edc416db21f6
arturgie <gubala-artur@wp.pl>
2025-04-16 21:36:07 +0200

    Use StatisticCard component in StatisticScreen.

--- app/src/main/java/com/scribbledash/statistic/StatisticScreen.kt
+++ app/src/main/java/com/scribbledash/statistic/StatisticScreen.kt
@@ -1,10 +1,15 @@
 package com.scribbledash.statistic
 
+import android.R.attr.shape
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -15,6 +20,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.scribbledash.R
 import com.scribbledash.core.presentation.components.ScribbleDashTopAppBar
+import com.scribbledash.statistic.components.StatisticCard
+import com.scribbledash.statistic.components.StatisticCardIcon
 
 @Composable
 internal fun StatisticRoute(
@@ -45,10 +52,36 @@ private fun StatisticScreen() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
-                .padding(horizontal = 16.dp)
+                .padding(start = 16.dp, end = 16.dp, top = 19.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-
+            Spacer(modifier = Modifier.height(19.dp))
+            StatisticCard(
+                leadingIcon = {
+                    StatisticCardIcon(
+                        icon = R.drawable.ic_hourglass,
+                        modifier = Modifier.background(
+                            color = Color(0x1A742EFC),
+                            shape = RoundedCornerShape(12.dp)
+                        )
+                    )
+                },
+                trackedDescription = "Nothing to track...for now",
+                trackedValue = "87%"
+            )
+            StatisticCard(
+                leadingIcon = {
+                    StatisticCardIcon(
+                        icon = R.drawable.ic_bolt,
+                        modifier = Modifier.background(
+                            color = Color(0x1A01D2FC),
+                            shape = RoundedCornerShape(12.dp)
+                        )
+                    )
+                },
+                trackedDescription = "Nothing to track...for now",
+                trackedValue = "156"
+            )
         }
     }
 }
-
