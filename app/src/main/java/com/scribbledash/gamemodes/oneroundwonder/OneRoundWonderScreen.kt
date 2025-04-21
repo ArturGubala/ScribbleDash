1e4694e1a235ad792aaed19a2bbecbe1cf01a09f
arturgie <gubala-artur@wp.pl>
2025-04-21 22:29:05 +0200

    Divide OneRoundWonderScreen for preview and normal view.

--- app/src/main/java/com/scribbledash/gamemodes/oneroundwonder/OneRoundWonderScreen.kt
+++ app/src/main/java/com/scribbledash/gamemodes/oneroundwonder/OneRoundWonderScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,6 +30,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.scribbledash.R
+import com.scribbledash.core.presentation.components.CountdownText
 import com.scribbledash.core.presentation.components.ScribbleDashScreenTitle
 import com.scribbledash.core.presentation.components.ScribbleDashTopAppBar
 import com.scribbledash.core.presentation.utils.ObserveAsEvents
@@ -57,6 +59,10 @@ internal fun OneRoundWonderRoute(
         }
     }
 
+    LaunchedEffect(Unit) {
+        viewModel.onAction(OneRoundWonderAction.ShowPreview)
+    }
+
     OneRoundWonderScreen(
         onBackClick = { viewModel.onAction(OneRoundWonderAction.OnBackClicked) },
         state = state,
@@ -70,6 +76,8 @@ private fun OneRoundWonderScreen(
     state: OneRoundWonderState,
     onAction: (OneRoundWonderAction) -> Unit
 ) {
+    val canDrawing = !state.isPreviewVisible
+
     Scaffold(
         topBar = {
             ScribbleDashTopAppBar(
@@ -95,56 +103,84 @@ private fun OneRoundWonderScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
-                .padding(start = 16.dp, end = 16.dp, bottom = 24.dp)
+                .padding(start = 16.dp, end = 16.dp, top = 53.dp, bottom = 24.dp)
         ) {
-            Spacer(modifier = Modifier.height(53.dp))
-            ScribbleDashScreenTitle(
-                headline = {
-                    Text(
-                        text = stringResource(R.string.time_to_draw),
-                        style = MaterialTheme.typography.titleLarge,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
+            if (state.isPreviewVisible) {
+                ScribbleDashScreenTitle(
+                    headline = {
+                        Text(
+                            text = stringResource(R.string.ready_set),
+                            style = MaterialTheme.typography.titleLarge,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                ScribbleDashDrawingArea(
+                    currentPath = state.currentPath,
+                    paths = state.paths,
+                    onAction = onAction,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    canDrawing = canDrawing
+                )
+                Spacer(modifier = Modifier.height(192.dp))
+                CountdownText(state.remainingTime)
+            }
+            else {
+                ScribbleDashScreenTitle(
+                    headline = {
+                        Text(
+                            text = stringResource(R.string.time_to_draw),
+                            style = MaterialTheme.typography.titleLarge,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                ScribbleDashDrawingArea(
+                    currentPath = state.currentPath,
+                    paths = state.paths,
+                    onAction = onAction,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    canDrawing = canDrawing
+                )
+                Spacer(modifier = Modifier.height(192.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    ScribbleDashIconButton(
+                        icon = R.drawable.ic_reply,
+                        onClick = { onAction(OneRoundWonderAction.OnUndoClick) },
+                        modifier = Modifier
+                            .size(64.dp),
+                        isActive = state.isUndoButtonActive
                     )
-                },
-                modifier = Modifier.fillMaxWidth(),
-            )
-            Spacer(modifier = Modifier.height(32.dp))
-            ScribbleDashDrawingArea(
-                currentPath = state.currentPath,
-                paths = state.paths,
-                onAction = onAction,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            )
-            Spacer(modifier = Modifier.height(192.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                ScribbleDashIconButton(
-                    icon = R.drawable.ic_reply,
-                    onClick = { onAction(OneRoundWonderAction.OnUndoClick) },
-                    modifier = Modifier
-                        .size(64.dp),
-                    isActive = state.isUndoButtonActive
-                )
-                ScribbleDashIconButton(
-                    icon = R.drawable.ic_forward,
-                    onClick = { onAction(OneRoundWonderAction.OnRedoClick) },
-                    modifier = Modifier
-                        .size(64.dp),
-                    isActive = state.isRedoButtonActive
-                )
-                ScribbleDashButton(
-                    description = stringResource(R.string.clear_canvas),
-                    onClick = { onAction(OneRoundWonderAction.OnClearCanvasClick) },
-                    modifier = Modifier
-                        .width(201.dp)
-                        .height(64.dp),
-                    isActive = state.isClearCanvasButtonActive
-                )
+                    ScribbleDashIconButton(
+                        icon = R.drawable.ic_forward,
+                        onClick = { onAction(OneRoundWonderAction.OnRedoClick) },
+                        modifier = Modifier
+                            .size(64.dp),
+                        isActive = state.isRedoButtonActive
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    ScribbleDashButton(
+                        description = stringResource(R.string.done),
+                        onClick = { onAction(OneRoundWonderAction.OnClearCanvasClick) },
+                        modifier = Modifier
+                            .width(124.dp)
+                            .height(64.dp),
+                        isActive = state.isClearCanvasButtonActive
+                    )
+                }
             }
         }
     }
