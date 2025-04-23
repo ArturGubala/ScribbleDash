711802b008d9e32ee4f2d96660fcd27b405d7a84
arturgie <gubala-artur@wp.pl>
2025-04-23 22:40:48 +0200

    Move drawing to raw directory and update DrawingsExtensions.

--- app/src/main/java/com/scribbledash/core/presentation/utils/DrawingsExtensions.kt
+++ app/src/main/java/com/scribbledash/core/presentation/utils/DrawingsExtensions.kt
@@ -3,40 +3,40 @@ package com.scribbledash.core.presentation.utils
 import com.scribbledash.R
 import com.scribbledash.core.domain.model.Drawings
 
-fun getDrawableIdForDrawing(drawing: Drawings): Int {
+fun getdrawingIdForDrawing(drawing: Drawings): Int {
     return when (drawing) {
-        Drawings.ALIEN -> R.drawable.img_alien
-        Drawings.BICYCLE -> R.drawable.img_bicycle
-        Drawings.BOAT -> R.drawable.img_boat
-        Drawings.BOOK -> R.drawable.img_book
-        Drawings.BUTTERFLY -> R.drawable.img_butterfly
-        Drawings.CAMERA -> R.drawable.img_camera
-        Drawings.CAR -> R.drawable.img_car
-        Drawings.CASTLE -> R.drawable.img_castle
-        Drawings.CAT -> R.drawable.img_cat
-        Drawings.CLOCK -> R.drawable.img_clock
-        Drawings.CROWN -> R.drawable.img_crown
-        Drawings.CUP -> R.drawable.img_cup
-        Drawings.DOG -> R.drawable.img_dog
-        Drawings.ENVELOPE -> R.drawable.img_envelope
-        Drawings.EYE -> R.drawable.img_eye
-        Drawings.FISH -> R.drawable.img_fish
-        Drawings.FLOWER -> R.drawable.img_flower
-        Drawings.FOOTBALL_FIELD -> R.drawable.img_football_field
-        Drawings.FROG -> R.drawable.img_frog
-        Drawings.GLASSES -> R.drawable.img_alien
-        Drawings.HEART -> R.drawable.img_heart
-        Drawings.HELICOTPER -> R.drawable.img_helicotper
-        Drawings.HOTAIRBALLOON -> R.drawable.img_hotairballoon
-        Drawings.HOUSE -> R.drawable.img_house
-        Drawings.MOON -> R.drawable.img_moon
-        Drawings.MOUNTAINS -> R.drawable.img_mountains
-        Drawings.ROBOT -> R.drawable.img_robot
-        Drawings.ROCKET -> R.drawable.img_rocket
-        Drawings.SMILEY -> R.drawable.img_smiley
-        Drawings.SNOWFLAKE -> R.drawable.img_snowflake
-        Drawings.SOFA -> R.drawable.img_sofa
-        Drawings.TRAIN -> R.drawable.img_train
-        Drawings.UMBRELLA -> R.drawable.img_umbrella
-        Drawings.WHALE -> R.drawable.img_whale
+        Drawings.ALIEN -> R.raw.img_alien
+        Drawings.BICYCLE -> R.raw.img_bicycle
+        Drawings.BOAT -> R.raw.img_boat
+        Drawings.BOOK -> R.raw.img_book
+        Drawings.BUTTERFLY -> R.raw.img_butterfly
+        Drawings.CAMERA -> R.raw.img_camera
+        Drawings.CAR -> R.raw.img_car
+        Drawings.CASTLE -> R.raw.img_castle
+        Drawings.CAT -> R.raw.img_cat
+        Drawings.CLOCK -> R.raw.img_clock
+        Drawings.CROWN -> R.raw.img_crown
+        Drawings.CUP -> R.raw.img_cup
+        Drawings.DOG -> R.raw.img_dog
+        Drawings.ENVELOPE -> R.raw.img_envelope
+        Drawings.EYE -> R.raw.img_eye
+        Drawings.FISH -> R.raw.img_fish
+        Drawings.FLOWER -> R.raw.img_flower
+        Drawings.FOOTBALL_FIELD -> R.raw.img_football_field
+        Drawings.FROG -> R.raw.img_frog
+        Drawings.GLASSES -> R.raw.img_glasses
+        Drawings.HEART -> R.raw.img_heart
+        Drawings.HELICOTPER -> R.raw.img_helicotper
+        Drawings.HOTAIRBALLOON -> R.raw.img_hotairballoon
+        Drawings.HOUSE -> R.raw.img_house
+        Drawings.MOON -> R.raw.img_moon
+        Drawings.MOUNTAINS -> R.raw.img_mountains
+        Drawings.ROBOT -> R.raw.img_robot
+        Drawings.ROCKET -> R.raw.img_rocket
+        Drawings.SMILEY -> R.raw.img_smiley
+        Drawings.SNOWFLAKE -> R.raw.img_snowflake
+        Drawings.SOFA -> R.raw.img_sofa
+        Drawings.TRAIN -> R.raw.img_train
+        Drawings.UMBRELLA -> R.raw.img_umbrella
+        Drawings.WHALE -> R.raw.img_whale
     }}
