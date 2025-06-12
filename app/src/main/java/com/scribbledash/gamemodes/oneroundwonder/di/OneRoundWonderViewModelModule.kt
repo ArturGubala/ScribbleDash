6325c1ef6418cd9399306cff87121912e2d1cfd8
arturgie <gubala-artur@wp.pl>
2025-06-12 21:47:04 +0200

    Add VectorXmlParser to OneRoundWonderViewModelModule.

--- app/src/main/java/com/scribbledash/gamemodes/oneroundwonder/di/OneRoundWonderViewModelModule.kt
+++ app/src/main/java/com/scribbledash/gamemodes/oneroundwonder/di/OneRoundWonderViewModelModule.kt
@@ -1,9 +1,11 @@
 package com.scribbledash.gamemodes.oneroundwonder.di
 
 import com.scribbledash.gamemodes.oneroundwonder.OneRoundWonderViewModel
+import com.scribbledash.gamemodes.oneroundwonder.utils.VectorXmlParser
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val oneRoundWonderViewModelModule = module {
     viewModelOf(::OneRoundWonderViewModel)
+    single { VectorXmlParser(get()) }
 }
