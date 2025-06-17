d527a38886b089cf8a7dc686d2edd8ffc0e90b2e
arturgie <gubala-artur@wp.pl>
2025-06-17 23:04:11 +0200

    Refactor BitmapExtensions fun.

--- app/src/main/java/com/scribbledash/gameplay/utils/BitmapExtensions.kt
+++ app/src/main/java/com/scribbledash/gameplay/utils/BitmapExtensions.kt
@@ -6,27 +6,41 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
+import java.io.IOException
 
-fun Bitmap.saveToFile(context: Context, bitmap: Bitmap, fileName: String): Uri? {
+fun Bitmap.saveToGallery(
+    context: Context,
+    fileName: String,
+    format: Bitmap.CompressFormat = Bitmap.CompressFormat.PNG,
+    quality: Int = 100
+): Uri? {
     val resolver = context.contentResolver
     val contentValues = ContentValues().apply {
         put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
-        put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        put(MediaStore.Images.Media.MIME_TYPE, when (format) {
+            Bitmap.CompressFormat.JPEG -> "image/jpeg"
+            Bitmap.CompressFormat.PNG -> "image/png"
+            Bitmap.CompressFormat.WEBP -> "image/webp"
+            else -> "image/png"
+        })
         put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
         put(MediaStore.Images.Media.IS_PENDING, 1)
     }
 
-    val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+    return try {
+        val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        uri?.let {
+            resolver.openOutputStream(it)?.use { outputStream ->
+                this.compress(format, quality, outputStream)
+            }
 
-    imageUri?.let { uri ->
-        resolver.openOutputStream(uri).use { outputStream ->
-            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream!!)
+            contentValues.clear()
+            contentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
+            resolver.update(it, contentValues, null, null)
         }
-
-        contentValues.clear()
-        contentValues.put(MediaStore.Images.Media.IS_PENDING, 0)
-        resolver.update(uri, contentValues, null, null)
+        uri
+    } catch (e: IOException) {
+        e.printStackTrace()
+        null
     }
-
-    return imageUri
 }
