59ee230f25e8127e4a67b8a31bb2b37cd005f3b8
arturgie <gubala-artur@wp.pl>
2025-06-20 23:18:14 +0200

    Add BitmapExtension to di module.

--- app/src/main/java/com/scribbledash/gameplay/di/GameplayViewModelModule.kt
+++ app/src/main/java/com/scribbledash/gameplay/di/GameplayViewModelModule.kt
@@ -1,6 +1,7 @@
 package com.scribbledash.gameplay.di
 
 import com.scribbledash.gameplay.presentation.GameplayViewModel
+import com.scribbledash.gameplay.utils.BitmapExtensions
 import com.scribbledash.gameplay.utils.VectorXmlParser
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val gameplayViewModelModule = module {
     viewModelOf(::GameplayViewModel)
     single { VectorXmlParser(get()) }
+    single { BitmapExtensions(get()) }
 }
