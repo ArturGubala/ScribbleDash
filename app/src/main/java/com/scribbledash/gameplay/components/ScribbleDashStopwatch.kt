46d9b037f9d29240e087c90fe854f6b6173607f5
arturgie <gubala-artur@wp.pl>
2025-07-07 21:15:32 +0200

    Refactor ScribbleDashStopwatch.

--- app/src/main/java/com/scribbledash/gameplay/components/ScribbleDashStopwatch.kt
+++ app/src/main/java/com/scribbledash/gameplay/components/ScribbleDashStopwatch.kt
@@ -1,19 +1,23 @@
 package com.scribbledash.gameplay.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.scribbledash.R
-import com.scribbledash.core.presentation.components.ScribbleDashScreenTitle
 import com.scribbledash.ui.theme.BagelFatOne
 import com.scribbledash.ui.theme.ScribbleDashTheme
 import com.scribbledash.ui.theme.Text
@@ -22,36 +26,50 @@ import com.scribbledash.ui.theme.Text
 @Composable
 fun TimerDisplay(
     remainingTimeMs: Long,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     val totalSeconds = remainingTimeMs / 1000
     val minutes = totalSeconds / 60
     val seconds = totalSeconds % 60
     val timeText = String.format("%d:%02d", minutes, seconds)
 
-    ScribbleDashScreenTitle(
-        headline = {
-            Text(
-                text = timeText,
-                style = TextStyle(
-                    fontFamily = BagelFatOne,
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 14.sp,
-                    lineHeight = 18.sp,
-                    color = Text
-                ),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
-            )
-        },
-        modifier = Modifier.fillMaxWidth(),
-    )
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = timeText,
+            style = TextStyle(
+                fontFamily = BagelFatOne,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                lineHeight = 18.sp,
+                color = Text
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview
 @Composable
 fun TimerDisplayPreview() {
     ScribbleDashTheme {
-        TimerDisplay(220000L)
+        TimerDisplay(
+            remainingTimeMs = 220000L,
+            modifier = Modifier
+                .size(48.dp)
+                .shadow(
+                    elevation = 8.dp,
+                    shape = RoundedCornerShape(50.dp),
+                    ambientColor = Color(0x1F726558),
+                    spotColor = Color(0x1F726558)
+                )
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(16.dp)
+                )
+        )
     }
 }
