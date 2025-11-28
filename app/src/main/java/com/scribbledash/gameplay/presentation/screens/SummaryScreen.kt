7aed946c4ad5d22b1c34bc3b8f79489b3ee3b161
arturgie <gubala-artur@wp.pl>
2025-11-28 22:57:05 +0100

    Implement base view of SummaryScreen.

---
+++ app/src/main/java/com/scribbledash/gameplay/presentation/screens/SummaryScreen.kt
@@ -0,0 +1,140 @@
+package com.scribbledash.gameplay.presentation.screens
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import com.scribbledash.R
+import com.scribbledash.core.presentation.components.ScribbleDashTopAppBar
+import com.scribbledash.core.presentation.utils.GameType
+import com.scribbledash.gameplay.components.ScribbleDashButton
+import com.scribbledash.gameplay.components.ScribbleDashHighScoreBanner
+import com.scribbledash.gameplay.components.ScribbleDashScoreCard
+import com.scribbledash.gameplay.navigation.GAMEPLAY_GRAPH_ROUTE
+import com.scribbledash.gameplay.presentation.GameplayAction
+import com.scribbledash.gameplay.presentation.GameplayState
+import com.scribbledash.gameplay.presentation.GameplayViewModel
+import com.scribbledash.gameplay.utils.sharedViewModel
+import com.scribbledash.ui.theme.ScribbleDashTheme
+
+@Composable
+internal fun SummaryRoute(
+    navController: NavController
+) {
+    val viewModel: GameplayViewModel = sharedViewModel(navController, GAMEPLAY_GRAPH_ROUTE)
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    BackHandler {
+        viewModel.onAction(GameplayAction.OnBackClicked)
+    }
+    
+}
+
+@Composable
+private fun SummaryScreen(
+    onBackClick: () -> Unit,
+    state: GameplayState,
+) {
+    Scaffold(
+        topBar = {
+            ScribbleDashTopAppBar(
+                trailingContent = {
+                    IconButton(
+                        onClick = onBackClick
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_round_cross),
+                            contentDescription = null,
+                            tint = Color(0xFFA5978A)
+                        )
+                    }
+                }
+            )
+        },
+        containerColor = Color.Transparent,
+        modifier = Modifier
+            .background(Color(0xFFFEFAF6))
+            .systemBarsPadding()
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 23.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(
+                text = "Time’s up!",
+                style = MaterialTheme.typography.labelLarge
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.TopCenter
+            ) {
+                ScribbleDashScoreCard(
+                    score = 45,
+                    feedbackTitle = "Meh",
+                    feedbackDescription = "This is what happens when you let a cat hold the pencil!",
+                    isHighScore = false
+                )
+                ScribbleDashHighScoreBanner(
+                    modifier = Modifier.offset(y = -(10).dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+            ScribbleDashButton(
+                description = "DRAW AGAIN",
+                onClick = {
+                    when(state.gameType) {
+                        GameType.ONE_ROUND_WONDER -> TODO()
+                        GameType.SPEED_DRAW -> TODO()
+                        GameType.ENDLESS_MODE -> TODO()
+                    }
+                },
+                buttonColor = Color(0xFF238CFF),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp),
+                isActive = true
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ResultScreenPreview() {
+    ScribbleDashTheme {
+        SummaryScreen(
+          onBackClick = {},
+          state = GameplayState()
+        )
+    }
+}
