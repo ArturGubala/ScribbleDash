c9d352a346cf283979bba19d86fe6761f970b407
arturgie <gubala-artur@wp.pl>
2025-12-01 23:01:03 +0100

    Refactor ScribbleDashTopAppBar.

--- app/src/main/java/com/scribbledash/core/presentation/components/ScribbleDashTopAppBar.kt
+++ app/src/main/java/com/scribbledash/core/presentation/components/ScribbleDashTopAppBar.kt
@@ -3,13 +3,16 @@ package com.scribbledash.core.presentation.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -23,6 +26,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.scribbledash.R
+import com.scribbledash.gameplay.components.ScribbleDashDrawingCounter
 import com.scribbledash.gameplay.components.TimerDisplay
 import com.scribbledash.ui.theme.ScribbleDashTheme
 
@@ -38,25 +42,19 @@ fun ScribbleDashTopAppBar(
         modifier = modifier
             .fillMaxWidth()
             .requiredHeight(72.dp)
-            .let {
-                if (leadingContent != null || centerContent != null) {
-                    it.padding(horizontal = 16.dp)
-                } else {
-                    it.padding(horizontal = 16.dp, vertical = 21.dp)
-                }
-            }
+            .padding(horizontal = 16.dp)
     ) {
         Row(
             modifier = modifier.fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = if (leadingContent != null || centerContent != null) {
-                Arrangement.SpaceBetween
-            } else {
-                Arrangement.End
-            }
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            leadingContent?.invoke()
-            centerContent?.invoke()
+            if (leadingContent == null) {
+                Box(modifier = Modifier.size(48.dp)) {}
+            } else leadingContent.invoke()
+            if (centerContent == null) {
+                Spacer(modifier = Modifier.weight(1f))
+            } else centerContent.invoke()
             trailingContent()
         }
     }
@@ -66,36 +64,69 @@ fun ScribbleDashTopAppBar(
 @Composable
 fun ScribbleDashTopAppBarPreview() {
     ScribbleDashTheme {
-        ScribbleDashTopAppBar(
-            leadingContent = {
-
-                TimerDisplay(
-                    remainingTimeMs = 220000L,
-                    modifier = Modifier
-                        .size(48.dp)
-                        .shadow(
-                            elevation = 8.dp,
-                            shape = RoundedCornerShape(50.dp),
-                            ambientColor = Color(0x1F726558),
-                            spotColor = Color(0x1F726558)
+        Column(
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            ScribbleDashTopAppBar(
+                leadingContent =
+                    {
+                        TimerDisplay(
+                            remainingTimeMs = 220000L,
+                            modifier = Modifier
+                                .size(48.dp)
+                                .shadow(
+                                    elevation = 8.dp,
+                                    shape = RoundedCornerShape(50.dp),
+                                    ambientColor = Color(0x1F726558),
+                                    spotColor = Color(0x1F726558)
+                                )
+                                .background(
+                                    color = Color.White,
+                                    shape = RoundedCornerShape(16.dp)
+                                )
                         )
-                        .background(
-                            color = Color.White,
-                            shape = RoundedCornerShape(16.dp)
-                        )
-                )
-            },
-            trailingContent = {
-                IconButton(
-                    onClick = {  }
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_round_cross),
-                        contentDescription = null,
-                        tint = Color(0xFFA5978A)
+                    },
+                centerContent = {
+                    ScribbleDashDrawingCounter(
+                        value = 5.toString(),
+                        modifier = Modifier
+                            .width(76.dp)
                     )
+                },
+                trailingContent = {
+                    IconButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_round_cross),
+                            contentDescription = null,
+                            tint = Color(0xFFA5978A)
+                        )
+                    }
                 }
-            }
-        )
+            )
+
+            ScribbleDashTopAppBar(
+                leadingContent = null,
+                centerContent = {
+                    ScribbleDashDrawingCounter(
+                        value = 5.toString(),
+                        modifier = Modifier
+                            .width(76.dp)
+                    )
+                },
+                trailingContent = {
+                    IconButton(
+                        onClick = { }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_round_cross),
+                            contentDescription = null,
+                            tint = Color(0xFFA5978A)
+                        )
+                    }
+                }
+            )
+        }
     }
 }
