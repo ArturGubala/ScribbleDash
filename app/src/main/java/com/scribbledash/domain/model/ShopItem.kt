71e47ad3bd424e0ffb69201d7c96527d3f7b19b8
arturgie <gubala-artur@wp.pl>
2025-12-10 23:27:36 +0100

    Update ShopItem model.

--- app/src/main/java/com/scribbledash/domain/model/ShopItem.kt
+++ app/src/main/java/com/scribbledash/domain/model/ShopItem.kt
@@ -1,13 +1,18 @@
 package com.scribbledash.domain.model
 
+import androidx.annotation.DrawableRes
 import androidx.compose.ui.graphics.Color
 import com.scribbledash.core.presentation.utils.ShopItemTier
+import com.scribbledash.core.presentation.utils.ShopItemType
 
 data class ShopItem(
     val id: String,
     val category: ShopItemTier,
+    val type: ShopItemType,
     val previewColor: Color? = null,
-    val previewBackground: Color? = null,
+    val previewBackgroundColor: Color? = null,
+    // I know that domain should be pure kotlin but...
+    @param:DrawableRes val previewBackgroundImage: Int? = null,
     val isPurchased: Boolean = false,
     val cost: Int = 0
 )
